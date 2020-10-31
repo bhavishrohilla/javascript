f5300f5b75939ec36a8a674c71980d58c59a90f8
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2020-10-31 13:51:57 +0530

    updated files

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 if(4+3 === 7){
     alert("You are now online. Enjoy");
 }
+
+
+function sing() {
+    console.log("AHHHHHHHH");
+    console.log("TEEEEEE");
+}
+
+sing();
--- script2.js
+++ script2.js
@@ -3,3 +3,11 @@
 if(4+3 === 7){
     alert("Dont ever share your passwords to anyone to avoid Cyber Crime.THANK YOU !!");
 }
+
+function sing(song) {
+    console.log(song);
+}
+
+sing("laa laa laa laa")
+sing("heeyy heeyy heeyy heey")
+sing("daadaa daaada daaa")
