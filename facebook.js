753a37b26deef94cd758aae6ef7166c614397c43
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-01-15 15:41:06 +0530

    objects in datastructures

---
+++ facebook.js
@@ -0,0 +1,22 @@
+{
+    username:"bhavishrohilla",
+    password:"hello@123",
+}
+
+var database =[
+    {
+        username:"bhavishrohilla",
+        password:"hello@123",
+    }
+]
+
+var newsfeed = [
+    {
+    username:"Avish",
+    timeline:"If you are good at something never do it for FREE!!",
+    },
+    {
+        username:"bhavish",
+        timeline:"If you want to go to heaven,first you have to DIE!"
+    }
+]
---
+++ objects.js
@@ -0,0 +1,21 @@
+var user = {
+    name: "Bhavish",
+    age:21,
+    hobby:"Fitness Freak",
+    isMarried: false,
+    spells:["abrakadabra", "shazam", "boo"],
+    shout: function() {
+        console.log("Shouting!AHHH");
+    }
+};
+
+var list = [
+    {
+        username:"john",
+        password:"secret"
+    },
+    {
+        username:"jess",
+        password:"12345"
+    }
+];
