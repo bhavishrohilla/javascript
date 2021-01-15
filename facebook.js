3e7468d91b0f4b7908c45204736bc15ff8533310
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-01-15 20:03:50 +0530

    updated files

--- facebook.js
+++ facebook.js
@@ -1,22 +1,29 @@
-{
-    username:"bhavishrohilla",
-    password:"hello@123",
-}
-
 var database =[
     {
-        username:"bhavishrohilla",
-        password:"hello@123",
+        username: "bhavishrohilla",
+        password: "secret",
     }
-]
+];
 
 var newsfeed = [
     {
-    username:"Avish",
-    timeline:"If you are good at something never do it for FREE!!",
+    username: "Avish",
+    timeline: "If you are good at something never do it for FREE!!",
     },
     {
-        username:"bhavish",
-        timeline:"If you want to go to heaven,first you have to DIE!"
+        username: "bhavish",
+        timeline: "If you want to go to heaven,first you have to DIE!"
+    },
+];
+
+var usernamePrompt = prompt("What is your username");
+var passwordPrompt = prompt("What is your password");
+
+function signIn(user,pass) {
+    if (user === database[0].username && pass === database[0].password){
+        console.log(newsfeed);
+    } else{
+        alert("OOPS!! Wrong Credentials");
     }
-]
+}
+signIn(usernamePrompt,passwordPrompt);
